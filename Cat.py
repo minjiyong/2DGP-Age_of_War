62ea930b1170f0b713f132842e53d6a5070bbc97
minjiyong@home <jiyong2528@gmail.com>
2024-11-16 19:19:14 +0900

    하마 고양이 공속 조정

--- Cat.py
+++ Cat.py
@@ -14,7 +14,7 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 # default 아군 Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+FRAMES_PER_ACTION = 4
 
 
 class Attack:
@@ -33,7 +33,7 @@ class Attack:
         pass
     @staticmethod
     def draw(unit):
-        unit.image.clip_composite_draw(int(unit.frame) * 47, 166, 47, 55, 0, 'h', unit.x, unit.y, 50, 50)
+        unit.image.clip_composite_draw(int(unit.frame) * 47, 166, 47, 55, 0, 'h', unit.x, unit.y, 47, 55)
         pass
 
 class AutoRun:
